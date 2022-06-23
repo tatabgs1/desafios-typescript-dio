47c4eaf3b85ecd5aa6a60f51f720016323fe91ac
tatabgs1 <78986919+tatabgs1@users.noreply.github.com>
2022-06-23 14:14:45 -0300

    app.ts e app.js com o código do Desafio3
    o arquivo TypeScript, que tem a extensão .ts, ao ser compilado gera um arquivo .js (JavaScript). Essa característica permite que a aplicação desenvolvida seja executada em qualquer navegador sem que o desenvolvedor precise se preocupar com a compatibilidade em cada um.
    Aqui temos o arquivo app.ts e o app.js compilado.

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+exports.__esModule = true;
+var botaoAtualizar = document.getElementById('atualizar-saldo');
+var botaoLimpar = document.getElementById('limpar-saldo');
+var soma = document.getElementById('soma');
+var campoSaldo = document.getElementById('campo-saldo');
+var saldoTotal = 0;
+limparSaldo();
+function somarAoSaldo(soma) {
+    if (campoSaldo) {
+        saldoTotal += soma;
+        campoSaldo.innerHTML = saldoTotal.toString();
+        limparCampoSoma();
+    }
+}
+function limparCampoSoma() {
+    soma.value = "";
+}
+function limparSaldo() {
+    if (campoSaldo) {
+        saldoTotal = 0;
+        campoSaldo.innerHTML = saldoTotal.toString();
+    }
+}
+if (botaoAtualizar) {
+    botaoAtualizar.addEventListener('click', function () {
+        console.log(soma.value);
+        somarAoSaldo(Number(soma.value));
+    });
+}
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
